8114f617091c730511468d4d8929ca0758edcc03
Alexandre Bonnasseau <alexandre.bonnasseau@mappy.com>
2022-03-17 00:58:32 +0100

    Backup history

--- collect_history.sh
+++ collect_history.sh
@@ -2,16 +2,20 @@
 
 FREE_BIKES_URL=https://gbfs.cogobikeshare.com/gbfs/en/free_bike_status.json
 STATION_INFORMATION_URL=https://gbfs.cogobikeshare.com/gbfs/en/station_information.json
-REFRESH_FREQUENCY=60
+REFRESH_FREQUENCY=59
 HISTORY_DIR=history
+HISTORY_BACKUP_DIR=history_backup
+
+mkdir -p tmp
 while true
 do
 	d=$(date +"%Y-%m-%d_%H-%M-%S")
 	echo "At $d downloading :"
 	echo "$FREE_BIKES_URL"
 	mkdir -p "$HISTORY_DIR/$d"
-	curl $FREE_BIKES_URL > "$HISTORY_DIR/$d/free_bike_status.json"
-	echo "$STATION_INFORMATION_URL"
-	curl $STATION_INFORMATION_URL > "$HISTORY_DIR/$d/station_information.json"
+	mkdir -p "$HISTORY_BACKUP_DIR/$d"
+	curl $FREE_BIKES_URL > tmp/free_bike_status.json
+	cp tmp/free_bike_status.json "$HISTORY_BACKUP_DIR/$d/free_bike_status.json"
+	mv tmp/free_bike_status.json "$HISTORY_DIR/$d/free_bike_status.json"
 	sleep $REFRESH_FREQUENCY # 
 done 
